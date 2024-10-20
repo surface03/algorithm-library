29b761a2d01e5c802145ca5621334e4d92d1150a
surface03 <ginigini03@gmail.com>
2024-10-20 13:34:54 +0900

    modify count sqfree

--- number_theory/count_sqfree/count.cpp
+++ number_theory/count_sqfree/count.cpp
@@ -1,14 +1,15 @@
-#include <bits/stdc++.h>
-#define fi first
-#define se second
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-
+/**
+ * Counting Square-free Integers
+ * 
+ * Counting the number of square-free numbers between 1 and N
+ * N <= 10^18
+ * 
+ * Time Complexity: O(N^(1/3))
+ */
 
 vector<int> prime;
-bool notprime[50000010];
-int mobius[50000010];
+bool notprime[35000010];
+int mobius[35000010];
 unordered_map<int, ll> mp;
 
 ll SQRT(ll x){
@@ -18,7 +19,7 @@ ll SQRT(ll x){
 }
 
 ll Smob(int x){
-	if(x<=50000000)return mobius[x];
+	if(x<=35000000)return mobius[x];
 	//if(x<=0)return 0;
 	auto TT=mp.find(x);
 	if(TT!=mp.end())return TT->se;
@@ -30,27 +31,14 @@ ll Smob(int x){
 	return mp[x]=ans;
 }
 
-ll cnt(ll x){
-	ll ret=0;
-	ll xsq=SQRT(x);
-	ll t=min(1000000ll,xsq);
-	for(ll i=1;i<=t;i++){
-		ret+=(x/(i*i))*(mobius[i]-mobius[i-1]);
-	}
-	for(ll i=t+1,j;i<=xsq;i=j+1){
-		j=(ll)SQRT(x/(x/(i*i)));
-		ret+=(x/(i*i))*(Smob(j)-Smob(i-1));
-	}
-	return ret;
-}
-int main(){
+void preprocess(){
 	mobius[1]=1;
-	for(ll i=2;i<=50000000ll;i++){
+	for(ll i=2;i<=35000000ll;i++){
 		if(!notprime[i]){
 			prime.push_back(i);
 			mobius[i]=-1;
 		}
-		for(ll j=0;j<(int)prime.size()&&i*prime[j]<50000000ll;j++){
+		for(ll j=0;j<(int)prime.size()&&i*prime[j]<35000000ll;j++){
 			notprime[i*prime[j]]=true;
 			if(i%prime[j]==0){
 				break;
@@ -60,10 +48,23 @@ int main(){
 			}
 		}
 	}
-	for(ll i=2;i<=50000000ll;i++){
+	for(ll i=2;i<=35000000ll;i++){
 		mobius[i]+=mobius[i-1];
 	}
-	ll K;
-	cin>>K;
-	cout<<cnt(K)<<endl;
+}
+
+// Counting the number of square-free numbers between 1 and x
+// Call preprocess first!
+ll cnt(ll x){
+	ll ret=0;
+	ll xsq=SQRT(x);
+	ll t=min(2500000ll,xsq);
+	for(ll i=1;i<=t;i++){
+		ret+=(x/(i*i))*(mobius[i]-mobius[i-1]);
+	}
+	for(ll i=t+1,j;i<=xsq;i=j+1){
+		j=(ll)SQRT(x/(x/(i*i)));
+		ret+=(x/(i*i))*(Smob(j)-Smob(i-1));
+	}
+	return ret;
 }
