792aab3899a2f80ae41eb11247aaf25eb51e3142
surface03 <ginigini03@gmail.com>
2024-10-20 19:07:01 +0900

    modify

--- DynamicProgramming/Berlekamp/berlekamp.cpp
+++ DynamicProgramming/Berlekamp/berlekamp.cpp
@@ -1,53 +1,17 @@
-#ifdef LOCAL
+/**
+ * Berlekamp Massey Algorithm
+ * Original Code: https://gist.github.com/koosaga/d4afc4434dbaa348d5bef0d60ac36aa4
+ * 
+ * Input x에 대해 가장 짧은 dp 점화식 생성
+ * 각 input 및 output은 mod에 대한 나머지
+ * 
+ * Input x: dp 초기값
+ * ret: 선형 점화식 계수, return 되는 벡터
+ * dp[n]=ret[0]*dp[n-1]+ ... + ret[m-1]*dp[n-m]
+ * 
+ * O(SZ(x)^2)
+ */
 
-#include "debug.h"
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-#else
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-#define debug(...)
-#define debugArr(...)
-
-#endif
-
-
-using namespace std;
-using namespace __gnu_pbds;
-using ll = long long;
-using pii = pair<int,int>;
-
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-constexpr int mod=998244353;
-constexpr int inf=1000000000;
-constexpr ll MOD=998244353;
-constexpr ll INF=1000000000000000000ll;
-#define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr)
-#define fi first
-#define se second
-#define pb push_back
-#define SZ(x) ((int)((x).size()))
-#define all(x) (x).begin(),(x).end()
-
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-mt19937_64 rngll(chrono::steady_clock::now().time_since_epoch().count());
-
-template<typename T>
-T randint(T __L, T __R) {return uniform_int_distribution<T>(__L,__R)(rngll);}
-
-ll ipow(ll _a,ll _b){_a %= MOD;ll _ret=1;while(_b){if(_b&1) _ret=_ret*_a % MOD;_a=_a*_a % MOD;_b>>=1;}return _ret;}
-ll ipowM(ll _a,ll _b,ll _MM){_a %= _MM;ll _ret=1%_MM;while(_b){if(_b&1) _ret=_ret*_a % _MM;_a=_a*_a % _MM;_b>>=1;}return _ret;}
-
-
-
-// x: dp 초기값
-// ret: 선형 점화식 계수, return 되는 벡터
-// dp[n]=ret[0]*dp[n-1]+ ... + ret[m-1]*dp[n-m]
 vector<int> berlekamp_massey(vector<int> x){
 	vector<int> ls, cur;
 	int lf, ld;
@@ -78,17 +42,4 @@ vector<int> berlekamp_massey(vector<int> x){
 	}
 	for(auto &i : cur) i = (i % mod + mod) % mod;
 	return cur;
-}
-
-int main(){
-	int n;
-	cin>>n;
-	vector<int> T;
-	for(int i=1;i<=n;i++){
-		int x;cin>>x;
-		T.pb(x);
-	}
-	vector<int> ans = berlekamp_massey(T);
-	cout<<ans.size()<<endl;
-	for(int i:ans)cout<<i<<" ";
 }
