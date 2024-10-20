d0b5d1fb49b2099e046a921e78e33061183c9e7b
surface03 <ginigini03@gmail.com>
2024-10-20 19:25:27 +0900

    modify

--- NumberTheory/YarinSieve/yarin.cpp
+++ NumberTheory/YarinSieve/yarin.cpp
@@ -1,19 +1,27 @@
-// Super fast & Memory-tight Sieve by Yarin
+/**
+ * Super fast & Memory-tight Sieve by Yarin
+ * 
+ * Original Code: https://github.com/mejibyte/competitive_programming/blob/master/lib/Documentation/9thinksYouNeed-ACMSolver/sieve%20by%20yarin.cpp
+ */
 
-#define MAXSIEVE 100000000 // All prime numbers up to this 
-#define MAXSIEVEHALF (MAXSIEVE/2) 
-#define MAXSQRT 5000 // sqrt(MAXSIEVE)/2 
+// All prime numbers up to this
+constexpr int MAXSIEVE = 1000077376;
+constexpr int MAXSIEVEHALF = 500038688;
+
+// sqrt(MAXSIEVE)/2
+constexpr int MAXSQRT = 15812;
+
 char a[MAXSIEVE/16+2]; 
-#define isprime(n) (a[(n)>>4]&(1<<(((n)>>1)&7))) // Works when n is odd 
 
 //have to check for even numbers
-void sieve()
-{
-    int i,j; 
-    memset(a,255,sizeof(a)); 
-    a[0]=0xFE; 
-    for(i=1;i<MAXSQRT;i++) 
-        if (a[i>>3]&(1<<(i&7))) 
-            for(j=i+i+i+1;j<MAXSIEVEHALF;j+=i+i+1) 
-                a[j>>3]&=~(1<<(j&7));
+void yarin_sieve() {
+	int i,j; 
+	memset(a,255,sizeof(a)); 
+	a[0]=0xFE; 
+	for(i=1;i<MAXSQRT;i++) 
+		if (a[i>>3]&(1<<(i&7))) 
+			for(j=i+i+i+1;j<MAXSIEVEHALF;j+=i+i+1) 
+				a[j>>3]&=~(1<<(j&7));
 }
+
+inline bool is_prime_yarin(int n){ return (n%2)?(a[n>>4]&(1<<((n>>1)&7))):(n==2); }
