4eaa4a175c4334bd347f061ad97df1094c21921b
surface03 <ginigini03@gmail.com>
2024-11-08 01:11:17 +0900

    add ge

---
+++ Math/GaussianElimination/gaussian_elimination.cpp
@@ -0,0 +1,162 @@
+template <typename T>
+inline bool IsZero(T x) {
+	if constexpr (is_floating_point_v<T>) return std::abs(x) < 1e-9;
+	else return x == T(0);
+}
+
+template <typename T>
+inline T Add(T a, T b) {
+	if constexpr (is_integral_v<T>) {
+		T ret = a + b;
+		if (ret >= MOD) ret -= MOD;
+		return ret;
+	}
+	else return a + b;
+}
+
+template <typename T>
+inline T Sub(T a, T b) {
+	if constexpr (is_integral_v<T>) {
+		T res = a - b;
+		if (res < 0) res += MOD;
+		return res;
+	}
+	else return a - b;
+}
+
+template <typename T>
+inline T Mul(T a, T b) {
+	if constexpr (is_integral_v<T>) {
+		return T((1ll * a * b) % MOD);
+	}
+	else return a * b;
+}
+
+template <typename T>
+inline T Div(T a, T b) {
+	if constexpr (is_integral_v<T>) {
+		return Mul(a, ipow(b, MOD-2));
+	}
+	else return a / b;
+}
+
+
+// returns {rref, rank, det, inv}
+template <typename T, bool do_inv = false>
+tuple<vector<vector<T>>, int, T, vector<vector<T>>> Gaussian_Elimination(vector<vector<T>> a) {
+	int n = a.size(), m = a[0].size(), rank = 0;
+
+	vector<vector<T>> out;
+	T det = T(1);
+
+	if constexpr (do_inv){
+		out.resize(n, vector<T>(m, T(0)));
+		for (int i = 0; i < n; i++) out[i][i] = T(1);
+	}
+	
+	// set a[*][i], expect for one a[j][i]
+	for (int i = 0; i < m; i++) {
+		if (rank == n) break;
+		if (IsZero(a[rank][i])) {
+			int idx = -1;
+
+			// If T is floating-point type, use largest abs to handle precision.
+			if constexpr (is_floating_point_v<T>){
+				T mx = T(0);
+				for (int j = rank + 1; j < n; j++) {
+					T abs_val = std::abs(a[j][i]);
+					if (abs_val > mx) {
+						mx = abs_val;
+						idx = j;
+					}
+				}
+			}
+			else {
+				for (int j = rank + 1; j < n; j++) {
+					if (!IsZero(a[j][i])) {
+						idx = j;
+						break;
+					}
+				}
+			}
+			if (idx == -1 || IsZero(a[idx][i])) {
+				det = T(0);
+				continue;
+			}
+			for (int k = 0; k < m; k++) {
+				a[rank][k] = Add(a[rank][k], a[idx][k]);
+				if constexpr (do_inv) out[rank][k] = Add(out[rank][k], out[idx][k]);
+			}
+		}
+		det = Mul(det, a[rank][i]);
+		T coeff = Div(T(1), a[rank][i]);
+		for (int j = 0; j < m; j++) a[rank][j] = Mul(a[rank][j], coeff);
+		if constexpr (do_inv) {
+			for (int j = 0; j < m; j++) out[rank][j] = Mul(out[rank][j], coeff);
+		}
+		for (int j = 0; j < n; j++) {
+			if (rank == j) continue;
+			T t = a[j][i];
+			for (int k = 0; k < m; k++) a[j][k] = Sub(a[j][k], Mul(a[rank][k], t));
+			if constexpr (do_inv) {
+				for (int k = 0; k < m; k++) out[j][k] = Sub(out[j][k], Mul(out[rank][k], t));
+			}
+		}
+		rank++;
+	}
+	return {a, rank, det, out};
+}
+
+// returns {R, c, {basis[0], basis[1], ..., basis[R-1]}}
+// If linear system has no answer, R = -1
+template <typename T>
+tuple<int, vector<T>, vector<vector<T>>> solve_linear_system(vector<vector<T>> A, vector<T> b) {
+	int n = A.size();
+	int m = A[0].size();
+	vector<vector<T>> M(n, vector<T>(m + 1));
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			M[i][j] = A[i][j];
+		}
+		M[i][m] = b[i];
+	}
+
+	auto [rref, rank, det, out] = Gaussian_Elimination<T, false>(M);
+
+	for (int i = 0; i < n; ++i) {
+		if (IsZero(rref[i][m])) continue;
+		bool all_zero = true;
+		for (int j = 0; j < m; ++j) {
+			if (!IsZero(rref[i][j])) {
+				all_zero = false;
+				break;
+			}
+		}
+		if (all_zero) return { -1, {}, {} };
+	}
+
+	vector<int> cols;
+	for (int i = 0; i < n; i++) {
+		int piv_col = 0;
+		while (piv_col < m && IsZero(rref[i][piv_col])) piv_col++;
+		if (piv_col == m) break;
+		cols.push_back(piv_col);
+	}
+
+	// Compute particular solution
+	vector<T> sol(m, T(0));
+	for (int i = 0; i < SZ(cols); i++) sol[cols[i]] = Div(rref[i][m], rref[i][cols[i]]);
+
+	// Compute basis
+	vector<vector<T>> basis;
+	for (int i = 0; i < m; i++) {
+		if (binary_search(all(cols), i)) continue;
+
+		vector<T> bs(m, T(0));
+		bs[i] = T(1);
+		for(int j = 0; j < SZ(cols); j++) bs[cols[j]] = Sub(T(0), Div(rref[j][i], rref[j][cols[j]]));
+		basis.push_back(bs);
+	}
+
+	return {SZ(basis), sol, basis};
+}
