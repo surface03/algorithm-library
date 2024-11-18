c1c563d94d7bc85f25b9c76d850813bf1ba6e758
surface03 <ginigini03@gmail.com>
2024-11-19 01:09:49 +0900

    add bm

--- Graph/BipartiteMatching/bipartite_matching.cpp
+++ Graph/BipartiteMatching/bipartite_matching.cpp
@@ -27,11 +27,11 @@
  * 
  * 
  * 최소 버텍스 커버 - 모든 간선에 대해, 양 끝점 중 적어도 하나의 정점이 포함되는 최소 정점 부분집합
- * get_min_vertex_cover: {groupA/B, vertex}
+ * get_min_vertex_cover: {vertex, group A/B (0 or 1)}
  * 
  * 
  * 최대독립집합 - 어떤 정점도 인접하지 않은 최대 정점 부분집합 (최소 버텍스 커버의 여집합)
- * get_max_independent_set: {groupA/B, vertex}
+ * get_max_independent_set: {vertex, group A/B (0 or 1)}
  * 
  * 
  * 쾨니그의 정리 - 이분 그래프에서 최대매칭 == 최소 버텍스 커버
@@ -42,6 +42,9 @@
  * gen_max_match(): O(VE)
  * get_min_vertex_cover: O(VE)
  * get_max_independent_set: O(VE)
+ * 
+ * Original Code: https://github.com/Pentagon03/Algorithms/blob/master/Flow/BipartiteMatching.cpp
+ * Added more features
  */
 
 struct BipartiteMatching{
@@ -104,47 +107,33 @@ struct BipartiteMatching{
 	}
 
 	vector<pii> get_min_vertex_cover(){
-		if(pv == 0) max_matching();
-		vector<vector<int>> g2(size(B));
-		for(int i=0;i<size(A);i++){
-			for(int x:g[i]){
-				g2[x].push_back(i);
-			}
-		}
-		vector<bool> chkA(size(A)), chkB(size(B));
+		if(pv == 0) gen_max_match();
+		vector<bool> chkA(N+1), chkB(M+1);
+
 		function<void(int,int)> dfs2 = [&](int x,int team){
 			auto&chk = team?chkB:chkA;
 			if(chk[x]) return;
 			chk[x] = true;
-			auto&rg = team?g2:g;
-			for(int nx: rg[x])
+			auto&adj = team?rev_g:g;
+			for(int nx: adj[x])
 				if((team == 0 && A[x] != nx) || (team == 1 && B[x] == nx))
 					dfs2(nx, !team);
 		};
-		for(int i=0;i<size(A);i++)
-			if(A[i] == -1)
-				dfs2(i, 0);
+
+		for(int i=1;i<=N;i++) if(A[i] <= 0) dfs2(i, 0);
 		vector<pii> ans;
-		for(int i=0;i<size(A);i++)
-			if(!chkA[i])
-				ans.emplace_back(0, i);
-		for(int i=0;i<size(B);i++)
-			if(chkB[i])
-				ans.emplace_back(1, i);
+		for(int i=1;i<=N;i++) if(!chkA[i]) ans.emplace_back(i, 0);
+		for(int i=1;i<=M;i++) if(chkB[i]) ans.emplace_back(1, i);
 		return ans;
 	}
+	
 	vector<pii> get_max_independent_set(){
 		auto mvc = get_min_vertex_cover();
-		vector<bool> chkA(size(A)), chkB(size(B));
-		for(auto[c,x]: mvc)
-			(c?chkB:chkA)[x] = true;
+		vector<bool> chkA(N+1), chkB(M+1);
+		for(auto[x,c]: mvc) (c?chkB:chkA)[x] = true;
 		vector<pii> ans;
-		for(int i=0;i<size(A);i++)
-			if(!chkA[i])
-				ans.emplace_back(0, i);
-		for(int i=0;i<size(B);i++)
-			if(!chkB[i])
-				ans.emplace_back(1, i);
+		for(int i=1;i<=N;i++) if(!chkA[i]) ans.emplace_back(i, 0);
+		for(int i=1;i<=M;i++) if(!chkB[i]) ans.emplace_back(i, 1);
 		return ans;
 	}
 };
